cd8fb41194f897af8f7504138ddbd54b41273fc0
Student P_08 <studentp_08@Student-P-08.local>
2017-11-08 11:16:48 +0530

    Initial Commit

---
+++ day21_iOS_xmlParsing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day21_iOS_xmlParsing
+//
+//  Created by Student P_08 on 08/11/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ day21_iOS_xmlParsing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  day21_iOS_xmlParsing
+//
+//  Created by Student P_08 on 08/11/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
